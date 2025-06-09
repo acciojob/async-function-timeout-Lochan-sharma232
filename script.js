440ded1156c26c44324cade7261654a98a49d7d2
Lochan-sharma232 <lochansharma537@gmail.com>
2025-06-09 22:49:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //your JS code here. If required.
 const text=document.getElementById("text");
 const number=document.getElementById("delay");
+const btn=document.getElementById("btn");
 
+async function promise1(){
+	return new Promise((resolve,reject)=>{
+		set Timeout(await()=>{
+resolve("");			
+		},1000);
+	} )
+}
 
